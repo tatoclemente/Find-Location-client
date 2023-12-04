1b3ce37072947d40bd5f34e47a4047e1df3233a0
tatoclemente <tatoapuros@gmail.com>
2023-12-04 19:42:24 -0300

    Clean code

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,8 @@ import Map from './finder/page'
 
 export default function Home() {
 
-  // const apiKey = process.env.GOOGLE_MAPS_API_KEY
   return (
     <main className={styles.main}>
-      {/* <iframe
-        src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=-31.859856, -62.721137`}
-        width="600"
-        height="450"
-        frameborder="0"
-        allowfullscreen
-      ></iframe> */}
       <Map />
     </main>
   )
